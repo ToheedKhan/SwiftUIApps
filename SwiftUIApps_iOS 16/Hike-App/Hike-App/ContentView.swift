9ad3fb999472d74e320187b2a7877e65b0650cc1
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-21 18:47:12 +0530

    Hike App initial commit.

---
+++ SwiftUIApps_iOS 16/Hike-App/Hike-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hike-App
+//
+//  Created by Toheed Jahan Khan on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIApps_iOS 16/Hike-App/Hike-App/Hike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_AppApp.swift
+//  Hike-App
+//
+//  Created by Toheed Jahan Khan on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Hike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
