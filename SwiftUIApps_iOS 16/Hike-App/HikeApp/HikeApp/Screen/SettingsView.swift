d8524598c29b644861d7e57a1475a2752f9b83cb
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-22 19:00:12 +0530

    SwiftUI List and labeled content.

--- SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/Screen/SettingsView.swift
+++ SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/Screen/SettingsView.swift
@@ -77,6 +77,57 @@ struct SettingsView: View {
             // MARK: - SECTION: ICONS
             
             // MARK: - SECTION: ABOUT
+            
+            Section(
+                header: Text("ABOUT THE APP"),
+                footer: HStack {
+                    Spacer()
+                    Text("Copyright © All right reserved.")
+                    Spacer()
+                }
+                    .padding(.vertical, 8)
+            ) {
+                // 1. Basic Labeled Content
+                // LabeledContent("Application", value: "Hike")
+                
+                // 2. Advanced Labeled Content
+                /*LabeledContent {
+                 //Content
+                 Text("Hike")
+                 .foregroundColor(.primary)
+                 .fontWeight(.heavy)
+                 } label: {
+                 //Label
+                 HStack {
+                 ZStack {
+                 RoundedRectangle(cornerRadius: 8).frame(width: 30, height: 30)
+                 .foregroundColor(.blue)
+                 Image(systemName: "apps.iphone")
+                 .foregroundColor(.white)
+                 .fontWeight(.semibold)
+                 
+                 }
+                 Text("Application")
+                 }//: HStack
+                 }//: label
+                 */
+                
+                CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "HIKE", rowTintColor: .blue)
+                
+                CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "iOS, iPadOS", rowTintColor: .red)
+                
+                CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+                
+                CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+                
+                CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "John Doe", rowTintColor: .mint)
+                
+                CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
+                
+                CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Google", rowLinkDestination: "https://www.google.com")
+                
+            }//: Section
+            
         }//: List
     }
 }
