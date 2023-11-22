50fc5328373089f58dfc2ae2ab87dd5a5849d359
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-22 10:47:45 +0530

    Initial commit sample image display.

---
+++ SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HikeApp
+//
+//  Created by Toheed Jahan Khan on 22/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Toheed Jahan Khan on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
