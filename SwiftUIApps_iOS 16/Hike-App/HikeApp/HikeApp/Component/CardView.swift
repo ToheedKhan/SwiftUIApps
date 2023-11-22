b8230705d71894f6dd18acc73f7cd2b86df5fa22
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-22 13:52:53 +0530

    Implemented Card header View.

--- SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/Component/CardView.swift
+++ SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/Component/CardView.swift
@@ -37,25 +37,49 @@ struct CardView: View {
         
         ZStack {
           CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [
-                            .customGrayLight,
-                            .customGrayMedium],
-                                       startPoint: .top,
-                                       endPoint: .bottomTrailing
-                                      )
-                    )
-                    .frame(width: 256, height: 256)
+          
+          VStack {
+            // MARK: - HEADER
+            
+            VStack(alignment: .leading) {
+              HStack {
+                Text("Hiking")
+                  .fontWeight(.black)
+                  .font(.system(size: 52))
+                  .foregroundStyle(
+                    LinearGradient(
+                      colors: [
+                        .customGrayLight,
+                        .customGrayMedium],
+                      startPoint: .top,
+                      endPoint: .bottom)
+                  )
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
-            }
-        }//: CARD
+                Spacer()
+                
+                Button {
+                  // ACTION: Show a Sheet
+                  print("The button was pressed.")
+                  isShowingSheet.toggle()
+                } label: {
+                  CustomButtonView()
+                }
+                
+              }
+              
+              Text("Fun and enjoyable outdoor activity for friends and families.")
+                .multilineTextAlignment(.leading)
+                .italic()
+                .foregroundColor(.customGrayMedium)
+            } //: HEADER
+            .padding(.horizontal, 30)
+            
+           
+            
+          } //: ZSTACK
+        } //: CARD
         .frame(width: 320, height: 570)
-    }
+      }
 }
 
 struct CardView_Previews: PreviewProvider {
---
+++ SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/Component/CustomCircleView.swift
@@ -0,0 +1,36 @@
+//
+//  CustomCircleView.swift
+//  HikeApp
+//
+//  Created by Toheed Jahan Khan on 22/11/23.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    @State private var isAnimateGradient: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .customIndigoMedium,
+                            .customSalmonLight
+                        ],
+                        startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                        endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+                    )
+                )
+                
+        } //: ZSTACK
+        .frame(width: 256, height: 256)
+    }
+}
+
+struct CustomCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCircleView()
+    }
+}
