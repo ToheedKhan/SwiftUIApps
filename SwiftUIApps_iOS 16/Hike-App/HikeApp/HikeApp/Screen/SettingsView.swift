c1330b5e4643e67ff3966aa8cf0ff8b03f2f8fa9
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-22 21:16:00 +0530

    Alternate App Icons changes

--- SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/Screen/SettingsView.swift
+++ SwiftUIApps_iOS 16/Hike-App/HikeApp/HikeApp/Screen/SettingsView.swift
@@ -75,6 +75,47 @@ struct SettingsView: View {
             .listRowSeparator(.hidden)
             
             // MARK: - SECTION: ICONS
+            Section(header: Text("Alternate Icons")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        ForEach(alternateAppIcons.indices, id: \.self) { item in
+                            Button {
+                                print("Icon \(alternateAppIcons[item]) was pressed.")
+                             //To change app icon
+                                UIApplication.shared.setAlternateIconName("AppIcon-Campfire") { error in
+                                  if error != nil {
+                                    print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                                  } else {
+                                    print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                                  }
+                                }
+                            } label: {
+                                Image("\(alternateAppIcons[item])-Preview") //Preview icons size are smaller
+//                                Image("\(alternateAppIcons[item])")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(16)
+                        }.buttonStyle(.borderless)
+                        }//: For-Each
+                    }//: HStack
+                    /*
+                     In the last couple of years, developing swift UI applications, what I have found is that the list
+                     view does not particularly work seamlessly with buttons to ensure the button action works correctly.
+                     I highly recommend adding a borderless modifier to it.
+                     */
+                    
+                }//: ScrollView
+                .padding(.top, 12)
+                
+                Text("Choose your favourite app icon from the collection above.")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom, 12)
+                } //: SECTION
+               .listRowSeparator(.hidden)
             
             // MARK: - SECTION: ABOUT
             
