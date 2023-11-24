50cfbf1f7f82a67c1b4323b9361172e1d29750d8
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-24 19:25:48 +0530

    Haptic Feedback using UINotificationFeedbackGenerator.

---
+++ SwiftUIApps_iOS 16/Restart-App/Restart/README.md
@@ -0,0 +1,5 @@
+# Haptic Feedback.
+Haptics engages people's sense of touch to enhance the experience of interacting with on screen interfaces.
+We can play various types of haptics in addition to visual and auditory feedback to enhance touch gestures
+or toggling a switch.
+On top of that, we can use it to highlight, deny or confirm any other user interactions.
--- SwiftUIApps_iOS 16/Restart-App/Restart/Restart/Screens/OnboardingView.swift
+++ SwiftUIApps_iOS 16/Restart-App/Restart/Restart/Screens/OnboardingView.swift
@@ -21,6 +21,8 @@ struct OnboardingView: View {
     
     @State private var textTitle: String = "Share."
     
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     var body: some View {
         ZStack {
             Color("ColorBlue")
@@ -219,10 +221,12 @@ struct OnboardingView: View {
                                 }.onEnded{ _ in
                                     withAnimation(Animation.easeOut(duration: 0.4)) {
                                         if buttonOffset > buttonWidth / 2 {
+                                            hapticFeedback.notificationOccurred(.success)
                                             playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnboardingViewActive = false
                                         } else {
+                                            hapticFeedback.notificationOccurred(.warning)
                                             buttonOffset  = 0
                                         }
                                     }
