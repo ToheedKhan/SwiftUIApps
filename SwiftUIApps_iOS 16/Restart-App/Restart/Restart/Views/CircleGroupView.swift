2cc35cb7e5e8294abdb140057845449da41699a0
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-24 17:15:11 +0530

    Ring Animation

--- SwiftUIApps_iOS 16/Restart-App/Restart/Restart/Views/CircleGroupView.swift
+++ SwiftUIApps_iOS 16/Restart-App/Restart/Restart/Views/CircleGroupView.swift
@@ -19,6 +19,7 @@ struct CircleGroupView: View {
     
     @State var ShapeColor: Color
     @State var ShapeOpacity: Double
+    @State private var isAnimating: Bool = false
     
     // MARK: - BODY
     
@@ -34,6 +35,13 @@ struct CircleGroupView: View {
             
              
         } //: ZStack
+        .blur(radius: isAnimating ? 0 : 10)
+        .opacity(isAnimating ? 1 : 0)
+        .scaleEffect(isAnimating ? 1 : 0.5)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+        .onAppear(perform: {
+          isAnimating = true
+        })
     }
 }
 
