113ecf966e3e3e8f2d85eb2d2a37dcb6f7a03370
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-06 19:32:04 +0530

    App setup and folder structure.

---
+++ SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Toheed Jahan Khan on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/Screen/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  PinchApp
+//
+//  Created by Toheed Jahan Khan on 24/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //MARK: - Property
+    
+    //MARK: - FUNCTION
+    
+    //MARK: - CONTENT
+    
+    var body: some View {
+            Text("Hello, world!")
+        .padding()
+    }
+}
+
+//MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
