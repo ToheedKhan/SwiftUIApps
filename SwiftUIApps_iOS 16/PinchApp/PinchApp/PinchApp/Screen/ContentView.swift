75eafccc4ce43fd6f5e2bc96846ac47d0cbed848
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-07 19:39:19 +0530

    Implemented Drag gesture.

--- SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/Screen/ContentView.swift
+++ SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/Screen/ContentView.swift
@@ -12,21 +12,35 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     
     @State private var imageScale: CGFloat = 1
+    
+    @State private var imageOffset: CGSize = .zero
+    
+    
     //MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     //MARK: - CONTENT
     
     var body: some View {
         NavigationView {
             ZStack {
-               Image("magazine-front-cover")
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
+                
                 //MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
@@ -34,12 +48,25 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
-            }//: ZStack
+                
+                //MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded{ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
+            } //: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
