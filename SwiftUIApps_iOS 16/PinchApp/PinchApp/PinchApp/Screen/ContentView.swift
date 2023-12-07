e9d0b7c7c7152b0e9039505f335b9b0ee6c83559
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-07 13:44:29 +0530

    Added animation.

--- SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/Screen/ContentView.swift
+++ SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/Screen/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: - Property
-    
+    @State private var isAnimating: Bool = false
     //MARK: - FUNCTION
     
     //MARK: - CONTENT
@@ -23,9 +23,15 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
             }//: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
