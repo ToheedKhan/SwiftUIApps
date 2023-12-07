e95a3f9f65190a4aeb5b58fcff9f63ef7db83fae
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-07 12:18:17 +0530

    Implemented basic layout.

--- SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/Screen/ContentView.swift
+++ SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/Screen/ContentView.swift
@@ -15,8 +15,19 @@ struct ContentView: View {
     //MARK: - CONTENT
     
     var body: some View {
-            Text("Hello, world!")
-        .padding()
+        NavigationView {
+            ZStack {
+               Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+            }//: ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
