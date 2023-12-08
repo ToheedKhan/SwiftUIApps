aafed6f02735b6ba8247ca6bd0b7bf39e7f19eb5
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-08 18:51:19 +0530

    Added InfoPanelView.

---
+++ SwiftUIApps_iOS 16/PinchApp/PinchApp/PinchApp/View/InfoPanelView.swift
@@ -0,0 +1,58 @@
+//
+//  InfoPanelView.swift
+//  PinchApp
+//
+//  Created by Toheed Jahan Khan on 08/12/23.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var scale: CGFloat
+    var offset: CGSize
+    
+    var body: some View {
+        HStack {
+            // MARK: - HOTSPOT
+            
+            /*
+             This symbol activates the info panel after users long press on it.
+             Technically speaking, we won't use a swiftui button.
+             That's why I'm saying that this is a hotspot area.
+             */
+            Image(systemName: "circle.circle")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+            
+            Spacer()
+            
+            // MARK: - INFO PANEL
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView(scale: 1, offset: .zero)
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
--- SwiftUIApps_iOS 16/PinchApp/README.md
+++ SwiftUIApps_iOS 16/PinchApp/README.md
@@ -5,3 +5,20 @@
 You can move the two gray circles that represent your fingers around the screen by holding ** `Option` ** and ** `Shift` ** and moving the mouse.
 
 Pinch ** `Option` ** + click
+
+
+## SF SYMBOL
+
+There are 4 different rendering modes.
+**Monochrome **
+It applies one color to all symbols within a symbol paths render in the color we specify.
+**Hierarchical**
+This rendering mode assigns a different opacity of a single color to each layer, creating a visual hierarchy that gives depth to the symbol.
+**Palette**
+ which applies two or more contrasting colors to each symbol,
+using one color per layer, specifying only two colors.
+For a symbol that defines three levels of hierarchy means the secondary and tertiary layers use the
+same color.
+**Multi color**
+This is available for only a smaller pool of symbols.
+This multi color rendering applies system defined intrinsic colors to some symbols to enhance meaning.
