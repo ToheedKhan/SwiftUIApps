90af4f36b65678ca21e49b00e14d3ec657f5b6e5
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-14 12:09:47 +0530

    Implemented Fruit Card View.

--- SwiftUIApps_iOS 16/Fructus/Fructus/Fructus/View/FruitCardView.swift
+++ SwiftUIApps_iOS 16/Fructus/Fructus/Fructus/View/FruitCardView.swift
@@ -11,6 +11,8 @@ struct FruitCardView: View {
     
     // MARK: - PROPERTIES
     
+    //change its value to true each time when the card appears on the screen.
+    @State private var isAnimating: Bool = false
     
     // MARK: - BODY
     var body: some View {
@@ -26,13 +28,33 @@ struct FruitCardView: View {
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                /*when the value of the is animating property changes from false to true, then the initial
+                0.6% size of the image will be scaled up to its original size. */
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                
                 //FRUIT: TITLE
                 Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                    
                 //FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 400)
+                
                 //BUTTON: START
-
+                StartButtonView()
             } //: VSTACK
         } //: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
         /*
          To make this beautiful color gradient to expand itself to all edges of the screen, add a new frame modifier to this view and make its size not only flexible but expandable as well.
          Enter the following modifier at the top of the others because their order really matters in swift UI.
@@ -41,6 +63,7 @@ struct FruitCardView: View {
         //This is for background
         .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 
