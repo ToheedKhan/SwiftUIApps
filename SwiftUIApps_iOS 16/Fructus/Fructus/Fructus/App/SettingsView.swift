2572671f4da90f0b413eccf9c2c1890f7a0d5e1c
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-27 16:17:38 +0530

    Added toggle on SettingView.

--- SwiftUIApps_iOS 16/Fructus/Fructus/Fructus/App/SettingsView.swift
+++ SwiftUIApps_iOS 16/Fructus/Fructus/Fructus/App/SettingsView.swift
@@ -25,6 +25,8 @@ struct SettingsView: View {
      in the app's environment.
      */
     @Environment(\.presentationMode) var presentationMode
+    
+    //This property is use to restart the app
     @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     // MARK: - BODY
@@ -74,6 +76,23 @@ struct SettingsView: View {
                        */
                         .font(.footnote)
                         .multilineTextAlignment(.leading)
+                      
+                      Toggle(isOn: $isOnboarding) {
+                        if isOnboarding {
+                          Text("Restarted".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.green)
+                        } else {
+                          Text("Restart".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.secondary)
+                        }
+                      }
+                      .padding()
+                      .background(
+                        Color(UIColor.tertiarySystemBackground)
+                          .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                      )
                   }
                   
                   // MARK: - SECTION 3
