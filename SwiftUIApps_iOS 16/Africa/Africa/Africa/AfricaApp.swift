7e4e31df42ebfb5466503e81fabe54235faa6122
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-12-28 11:22:58 +0530

    Initial commit

---
+++ SwiftUIApps_iOS 16/Africa/Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Toheed Jahan Khan on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIApps_iOS 16/Africa/Africa/Africa/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Toheed Jahan Khan on 28/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
