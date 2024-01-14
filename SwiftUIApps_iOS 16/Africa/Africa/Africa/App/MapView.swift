428f5c928a80904085dcf2e4ad824db25cee56a3
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-01-14 09:12:38 +0530

    Created complex map with dynamic data. Added Annotation using old style using MapPin

--- SwiftUIApps_iOS 16/Africa/Africa/Africa/App/MapView.swift
+++ SwiftUIApps_iOS 16/Africa/Africa/Africa/App/MapView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     //MARK: - PROPERTIES
     
+    @State private var region: MKCoordinateRegion = {
+    var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+    var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+    var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+    
+    return mapRegion
+  }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
     //MARK: - BODY
 
     var body: some View {
-        NavigationView {
-            
-        } //: NAVIGATION
+        // MARK: - 1. Basic Map
+//        Map(coordinateRegion: $region)
+        
+        // MARK: - 2. ADVANCED MAP
+        /*
+         When we add an annotation to our map, then we have to provide some kind of content as well.
+         Mapping.
+         As for map annotations, Apple's Mapkit framework provides us multiple options that we can choose from.
+         */
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // (A) PIN: OLD STYLE (always static)
+            MapPin(coordinate: item.location, tint: .accentColor)
+        }) //: MAP
     }
 }
 
@@ -23,6 +43,6 @@ struct MapView: View {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView().previewDevice("iPhone 12 Pro")
     }
 }
