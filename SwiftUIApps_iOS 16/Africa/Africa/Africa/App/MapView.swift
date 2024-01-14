435f0927275c58d94d4f48952201244f9adaa255
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-01-14 09:48:13 +0530

    Created Information Panel above the map.

--- SwiftUIApps_iOS 16/Africa/Africa/Africa/App/MapView.swift
+++ SwiftUIApps_iOS 16/Africa/Africa/Africa/App/MapView.swift
@@ -53,6 +53,50 @@ struct MapView: View {
               MapAnnotationView(location: item)
             }
         }) //: MAP
+        // Information Panel above the map
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                  .resizable()
+                  .scaledToFit()
+              .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HSTACK - Latitude
+                    
+                    Divider()
+                    
+                    HStack {
+                      Text("Longitude:")
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .foregroundColor(.accentColor)
+                      Spacer()
+                      Text("\(region.center.longitude)")
+                        .font(.footnote)
+                        .foregroundColor(.white)
+                    } //: HSTACK - Longitude
+                } //: VSTACK
+            } //: HSTACK
+            .padding(.vertical, 12)
+            .padding(.horizontal, 16)
+            .background(
+              Color.black
+                .cornerRadius(8)
+                .opacity(0.6)
+            )
+            .padding()
+            , alignment: .top
+        ) //: OVERLAY
     }
 }
 
