593ed1a58031388e0cb4d5521bbdb66552f21fb4
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-01-15 12:43:11 +0530

    Complex grid layout with 4 toolbar buttons - Added code for toolbar buttons

--- SwiftUIApps_iOS 16/Africa/Africa/Africa/App/ContentView.swift
+++ SwiftUIApps_iOS 16/Africa/Africa/Africa/App/ContentView.swift
@@ -10,6 +10,34 @@ import SwiftUI
 struct ContentView: View {
     //MARK: - PROPERTIES
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    @State private var isGridViewActive: Bool = false
+    
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    @State private var gridLayout: [GridItem] = [ GridItem(.flexible()) ]
+    @State private var gridColumn: Int = 1
+    
+    //: MARK - FUNCTIONS
+    
+    func gridSwitch() {
+      gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+      gridColumn = gridLayout.count
+      print("Grid Number: \(gridColumn)")
+      
+      // TOOLBAR IMAGE
+      switch gridColumn {
+      case 1:
+        toolbarIcon = "square.grid.2x2"
+      case 2:
+        toolbarIcon = "square.grid.3x2"
+      case 3:
+        toolbarIcon = "rectangle.grid.1x2"
+      default:
+        toolbarIcon = "square.grid.2x2"
+      }
+    }
+    
     //MARK: - BODY
 
     var body: some View {
@@ -26,6 +54,32 @@ struct ContentView: View {
                 }
             } //: LIST
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+              ToolbarItem(placement: .navigationBarTrailing) {
+                HStack(spacing: 16) {
+                  // LIST
+                  Button(action: {
+                    print("List view is activated")
+                    isGridViewActive = false
+                  }) {
+                    Image(systemName: "square.fill.text.grid.1x2")
+                      .font(.title2)
+                      .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                  }
+                  
+                  // GRID
+                  Button(action: {
+                    print("Grid view is activated")
+                    isGridViewActive = true
+                    gridSwitch()
+                  }) {
+                    Image(systemName: toolbarIcon)
+                      .font(.title2)
+                      .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                  }
+                } //: HSTACK
+              } //: BUTTONS
+            } //: TOOLBAR
         } //: NAVIGATION
     }
 }
