0241c6c767e43de8d34514bdb6433a1b70f1749d
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-01-15 11:22:41 +0530

    PhotoGallery- Added tap gesture to change image upon selection.

--- SwiftUIApps_iOS 16/Africa/Africa/Africa/App/GalleryView.swift
+++ SwiftUIApps_iOS 16/Africa/Africa/Africa/App/GalleryView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct GalleryView: View {
     //MARK: - PROPERTIES
+    /* We need to create a new property that will hold the necessary information about the selected
+    animal above the grid.
+    */
+    @State private var selectedAnimal: String = "lion"
     
     let animals: [Animal] = Bundle.main.decode("animals.json")
     
@@ -26,17 +30,32 @@ struct GalleryView: View {
 
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                ForEach(animals) { item in
-                    // MARK: - IMAGE
-                    
-                    Image(item.image)
-                      .resizable()
-                      .scaledToFit()
-                      .clipShape(Circle())
-                      .overlay(Circle().stroke(Color.white, lineWidth: 8))
-                }
-            } //: GRID
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                
+                Image(selectedAnimal)
+                  .resizable()
+                  .scaledToFit()
+                  .clipShape(Circle())
+                  .overlay(Circle().stroke(Color.white, lineWidth: 8))
+            
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        // MARK: - IMAGE
+                        
+                        Image(item.image)
+                          .resizable()
+                          .scaledToFit()
+                          .clipShape(Circle())
+                          .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                          .onTapGesture {
+                              selectedAnimal = item.image
+                          }
+                    }
+                } //: GRID
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: SCROLLVIEW
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         //background motion Animation View.
