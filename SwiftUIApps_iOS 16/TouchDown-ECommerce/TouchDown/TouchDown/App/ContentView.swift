ce959feeb1e9100538c7274dbe598afe3dcf5a41
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-01-18 11:29:40 +0530

    Done changes to make status bar white but custom nav bar is covered by the notch at the top.

--- SwiftUIApps_iOS 16/TouchDown-ECommerce/TouchDown/TouchDown/App/ContentView.swift
+++ SwiftUIApps_iOS 16/TouchDown-ECommerce/TouchDown/TouchDown/App/ContentView.swift
@@ -13,9 +13,30 @@ struct ContentView: View {
     
     // MARK: - BODY
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
-    }
+        ZStack {
+            VStack(spacing: 0) {
+              NavigationBarView()
+                .padding()
+                .background(Color.white)
+                .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+              Spacer()
+                
+                  FooterView()
+                    .padding(.horizontal)
+              
+            } //: VSTACK
+            //By doing this, it will be white till top of the screen
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+       
+        } //: ZSTACK
+        /*All edges top with this ZStack will be the main content container from now so we can switch from
+         the e-commerce apps storefront home view to the product detail view later on.*/
+        /*
+         Afer this Status bar will be white but The custom nav bar is still covered by the ugly notch at the top.
+         */
+        .ignoresSafeArea(.all, edges: .top)
+      }
 }
 
 // MARK: - PREVIEW
