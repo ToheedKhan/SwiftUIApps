2189a351b81568ed3e80b40f6fae5fb8b25d07e1
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-02-01 12:21:56 +0530

    Created a custom header.

--- SwiftUIApps_iOS 16/Devote/Devote/Devote/View/ContentView.swift
+++ SwiftUIApps_iOS 16/Devote/Devote/Devote/View/ContentView.swift
@@ -66,6 +66,39 @@ struct ContentView: View {
                 // MARK: - MAIN VIEW
                 VStack {
                   // MARK: - HEADER
+                    HStack(spacing: 10) {
+                      // TITLE
+                      Text("Devote")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.heavy)
+                        .padding(.leading, 4)
+                      
+                      Spacer()
+                      
+                      // EDIT BUTTON
+                      EditButton()
+                        .font(.system(size: 16, weight: .semibold, design: .rounded))
+                        .padding(.horizontal, 10)
+                        .frame(minWidth: 70, minHeight: 24)
+                        .background(
+                          Capsule().stroke(Color.white, lineWidth: 2)
+                        )
+                      
+                      // APPEARANCE BUTTON
+                      Button(action: {
+                        // TOGGLE APPEARANCE
+
+                      }, label: {
+                        Image(systemName:  "moon.circle")
+                          .resizable()
+                          .frame(width: 24, height: 24)
+                          .font(.system(.title, design: .rounded))
+                      })
+                      
+                    } //: HSTACK
+                    .padding()
+                    .foregroundColor(.white)
+                    
                     Spacer(minLength: 80
                     )
                     //MARK: - NEW TASK BUTTON
