b82f2e51cb918dda33528b09fb5e6861cfaba337
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-02-02 15:43:48 +0530

    Added Checkbox component using ToggleStyle. Used Toggle element for preview.

---
+++ SwiftUIApps_iOS 16/Devote/Devote/Devote/Style/CheckboxStyle.swift
@@ -0,0 +1,35 @@
+//
+//  CheckboxStyle.swift
+//  Devote
+//
+//  Created by Toheed Jahan Khan on 02/02/24.
+//
+
+import SwiftUI
+
+struct CheckboxStyle: ToggleStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    return HStack {
+      Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+        .foregroundColor(configuration.isOn ? .pink : .primary)
+        .font(.system(size: 30, weight: .semibold, design: .rounded))
+        .onTapGesture {
+          configuration.isOn.toggle()
+      configuration.label
+    } //: HSTACK
+  }
+}
+
+struct CheckboxStyle_Previews: PreviewProvider {
+  static var previews: some View {
+      /*
+       Since we will develop a new style for swift UI's.
+       Toggle element.
+       Therefore, the best way to use the preview is to insert a new toggle element with our custom style.
+       */
+    Toggle("Placeholder label", isOn: .constant(false))
+      .toggleStyle(CheckboxStyle())
+      .padding()
+      .previewLayout(.sizeThatFits)
+  }
+}
