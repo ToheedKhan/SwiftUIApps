7b3ba124f19c70d20677aee685565c742b6f20dd
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-02-07 23:48:46 +0530

    Avocado project setup. Added app icons.

---
+++ SwiftUIApps_iOS 16/Avocados/Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Toheed Jahan Khan on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIApps_iOS 16/Avocados/README.md
@@ -0,0 +1,4 @@
+# Avocados
+
+For imagege in  inspector, resizing Preserve Vector data checkmark,
+make sures that our icons won't be pixelated and they will be as sharp as they were designed.
