87d5079c9976eab96d90f735b53ff426dfc76f2d
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-02-07 19:14:13 +0530

    Initial commit. Added app icons and colors.

---
+++ SwiftUIApps_iOS 16/LearnByDoing/LearnByDoing/LearnByDoing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearnByDoing
+//
+//  Created by Toheed Jahan Khan on 07/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIApps_iOS 16/LearnByDoing/LearnByDoing/LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by Toheed Jahan Khan on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
