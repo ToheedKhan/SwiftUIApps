2cf0e487461f785ca75cd122fd7259cb75fe38ba
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-02-21 14:30:55 +0530

    Created tabView.

---
+++ SwiftUIApps_iOS 16/Avocados/Avocados/Avocados/View/AvocadosView.swift
@@ -0,0 +1,20 @@
+//
+//  AvocadosView.swift
+//  Avocados
+//
+//  Created by Toheed Jahan Khan on 21/02/24.
+//
+
+import SwiftUI
+
+struct AvocadosView: View {
+    var body: some View {
+        Text("Avocados")
+    }
+}
+
+struct AvocadosView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadosView()
+    }
+}
