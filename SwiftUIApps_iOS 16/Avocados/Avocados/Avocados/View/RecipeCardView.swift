96b13d910dc3eaa4ea60eea9b0c69e8d65b569ce
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-02-21 19:13:52 +0530

    Created RecipeCookingView and RecipeRatingView in separate classes

--- SwiftUIApps_iOS 16/Avocados/Avocados/Avocados/View/RecipeCardView.swift
+++ SwiftUIApps_iOS 16/Avocados/Avocados/Avocados/View/RecipeCardView.swift
@@ -51,32 +51,10 @@ struct RecipeCardView: View {
                     .italic()
                 
                 // RATING
-                HStack(alignment: .center, spacing: 5) {
-                    ForEach(1...(recipe.rating), id: \.self) { _ in
-                        Image(systemName: "star.fill")
-                            .font(.body)
-                            .foregroundColor(Color.yellow)
-                    }
-                }
+                RecipeRatingView(recipe: recipe)
+
                 // COOKING
-                HStack(alignment: .center, spacing: 12) {
-                    HStack(alignment: .center, spacing: 2) {
-                        Image(systemName: "person.2")
-                        Text("Serves: \(recipe.serves)")
-                    }
-                    
-                    HStack(alignment: .center, spacing: 2) {
-                        Image(systemName: "clock")
-                        Text("Prep: \(recipe.preparation)")
-                    }
-                    
-                    HStack(alignment: .center, spacing: 2) {
-                        Image(systemName: "flame")
-                        Text("Cooking: \(recipe.cooking)")
-                    }
-                } //: HSTACK
-                .font(.footnote)
-                .foregroundColor(Color.gray)
+                RecipeCookingView(recipe: recipe)
                 
             } //: VSTACK
             
