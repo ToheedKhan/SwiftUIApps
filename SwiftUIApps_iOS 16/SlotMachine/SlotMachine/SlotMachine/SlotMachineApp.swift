14b0e7b955efab435be2dda6e7133bf94892a01c
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2024-02-22 17:01:03 +0530

    Initial commit. Project setup.

---
+++ SwiftUIApps_iOS 16/SlotMachine/SlotMachine/SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Toheed Jahan Khan on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
